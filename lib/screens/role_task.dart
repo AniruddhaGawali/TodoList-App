7a83c3123fb0e6efa54ad752625160c0960ce539
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2023-05-08 01:48:54 +0530

    fix small bug

--- lib/screens/role_task.dart
+++ lib/screens/role_task.dart
@@ -37,36 +37,35 @@ class RoleTaskScreen extends ConsumerWidget {
           title: Text(_getCapterlised(role)),
         ),
         body: SingleChildScrollView(
-          child: todos.isNotEmpty
-              ? Column(
-                  children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const FilterButtons(),
-                    TodoList(
-                        todos: ref
-                            .read(filterProvider.notifier)
-                            .getFilteredTodos(todos)),
-                  ],
-                )
-              : Center(
-                  child: Column(children: [
-                    Image.network(
-                        "https://img.freepik.com/free-vector/relaxing-home-concept-illustration_114360-1128.jpg?w=1480&t=st=1683480421~exp=1683481021~hmac=dc5360c1d18af237acf74b1086762e0f57c19a0c93161adf51597426d4e2c441"),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      'No Tasks Take a Break',
-                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                            color: Theme.of(context).colorScheme.primary,
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
-                  ]),
-                ),
-        ),
+            child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const FilterButtons(),
+            todos.isNotEmpty
+                ? TodoList(
+                    todos: ref
+                        .read(filterProvider.notifier)
+                        .getFilteredTodos(todos))
+                : Center(
+                    child: Column(children: [
+                      Image.network(
+                          "https://img.freepik.com/free-vector/relaxing-home-concept-illustration_114360-1128.jpg?w=1480&t=st=1683480421~exp=1683481021~hmac=dc5360c1d18af237acf74b1086762e0f57c19a0c93161adf51597426d4e2c441"),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        'No Tasks Here',
+                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ]),
+                  ),
+          ],
+        )),
       ),
       onWillPop: () async {
         ref.read(filterProvider.notifier).clear();
